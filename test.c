2759085c150a32f29b3729735b815fef092fdc47
zapperllp <krprem514@gmail.com>
2023-08-26 17:36:01 +0530

    new

--- test.c
+++ test.c
@@ -2,5 +2,17 @@
 
 int main(){
     printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
     return 0;
 }
