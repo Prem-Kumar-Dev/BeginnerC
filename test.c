a8524b376d7d1e305f6465d43f957ab31af0614a
zapperllp <krprem514@gmail.com>
2023-09-05 19:58:27 +0530

    1.

--- test.c
+++ test.c
@@ -5,20 +5,24 @@
 # include<stdlib.h>
 
 int main(){
-    char expression[]= "1+2+3-5*9*8/2";
+    char expression[100];
+    printf("Enter your equation:>> ");
+    scanf("%s",&expression);
     int i;
-    int result;
-    int num;
+    float result;
+    float num, cycle, placeholder;
     char sign;
 
     for(i=0;expression[i]!='\0';i++){
         char holder[2] = { expression[i],'\0'};
-
         if (i==0){
             result = atoi(holder);
+            placeholder=result;
         }
         if (holder[0]!='+' && holder[0]!='-' && holder[0]!='*' && holder[0]!='/'){
+            cycle=cycle+1;
             num = atoi(holder);
+            
             switch (sign)
             {
             case '+':
@@ -34,15 +38,21 @@ int main(){
                 result = result/num;
                 break;
             default:
+                if (cycle>1){
+                    result = (placeholder*10)+num;
+                    placeholder=num;
+                }
                 break;
             }
         }
+
         if (holder[0]=='+' || holder[0]=='-' || holder[0]=='*' || holder[0]=='/'){
             sign= holder[0];
+            cycle=0;
             }
-        printf(" %d", result );
         
     }
+    printf(" %f", result );
     return 0;
 }
 
